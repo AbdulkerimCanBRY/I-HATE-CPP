f1388ee496ec027ed87ecf30bb7b04d071cf8561
Abdulkerim022 <abdulkerimcan022@gmail.com>
2025-12-09 22:19:28 +0300

    Lesson-1
    We are adding our first lesson, may it be auspicious.

---
+++ Basic Information/Lesson-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>                                  // Giriş ve çıkış işlemleri için gerekli kütüphane
+using namespace std;                                 // Standart isim alanını kullanarak kodu sadeleştirir
+
+int main()                                           // Programın başlangıç noktası
+{                                                    
+    cout << "Hello World!" << endl;                  // Cout ile ekrana "Hello World!" yazdırır ve endl ile yeni satıra geçer
+    return 0;                                        // Programın başarılı bir şekilde sona erdiğini belirtir
+}
