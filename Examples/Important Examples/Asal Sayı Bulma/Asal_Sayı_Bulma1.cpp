9f83ed70f5e13ab9a2c9a8c8f981e6e687b0d59e
Abdulkerim022 <abdulkerimcan022@gmail.com>
2025-12-18 15:17:52 +0300

    Asal Sayı Bulma1
    Today we'll be tackling some challenging examples. Here, I've tried to explain a code for finding prime numbers. (There's another way, which I'll explain and share shortly.)

---
+++ Examples/Important Examples/Asal Sayı Bulma/Asal_Sayı_Bulma1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sayi, i, control;                                           // control değişkeni asal olup olmadığını kontrol etmek için kullanılır.
+    cout << "Pozitif bir tamsayi giriniz: ";
+    cin >> sayi;
+    if (sayi < 2) {                                                 // 2'den küçük sayılar asal sayı değildir.
+        cout << "Lütfen daha büyük bir sayı giriniz." << endl;
+        return 0;
+    }
+    for (i = 2; i <= sayi / 2; i++) {                               // Sayının yarısına kadar olan sayılarla bölünebilirlik kontrolü yapılır.
+        if (sayi % i == 0) {                                        // Eğer sayı herhangi bir sayıya tam bölünüyorsa asal değildir.
+            control=2;                                              // control değişkeni 2 olarak ayarlanır.
+            break;                                                  // Döngüden çıkılır.
+        }
+    }
+    if (control == 2) {                                             // Eğer control değişkeni 2 ise asal değildir. 
+        cout << sayi << " asal bir sayı değildir." << endl;        
+    } else {
+        cout << sayi << " asal bir sayıdır." << endl;
+    }
+}
