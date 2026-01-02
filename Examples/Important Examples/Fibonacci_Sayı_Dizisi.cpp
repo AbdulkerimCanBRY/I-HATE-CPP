d2084ee8962b754d19fe220d1d6d9aadb8add33e
Abdulkerim022 <abdulkerimcan022@gmail.com>
2026-01-02 23:51:18 +0300

    Correction
    I forgot to write the explanations.

--- Examples/Important Examples/Fibonacci_Sayı_Dizisi.cpp
+++ Examples/Important Examples/Fibonacci_Sayı_Dizisi.cpp
@@ -6,14 +6,14 @@ int main () {
     cout << "Fibonacci dizisinin eleman sayısını girin: ";
     cin >> n;
 
-    int a = 0, b = 1, c, i;
+    int a = 0, b = 1, c, i;              // Fibonacci dizisinin ilk iki elemanını tanımla
     cout << "Fibonacci Dizisi: " << a << ", " << b;
 
     for (i = 2; i < n; i++) {
-        c = a + b;
-        cout << ", " << c;
-        a = b;
-        b = c;
+        c = a + b;                       // Fibonacci dizisinin bir sonraki elemanını hesapla
+        cout << ", " << c;               // Hesaplanan elemanı ekrana yazdır
+        a = b;                           // Önceki iki elemanı güncelle
+        b = c;                           // Son iki elemanı güncelle
     }
 
     cout << endl;
