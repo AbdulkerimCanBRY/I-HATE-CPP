57f38656cefa2f8c13e605ee280116d9864dd6b1
Abdulkerim022 <abdulkerimcan022@gmail.com>
2026-01-03 22:06:10 +0300

    Ready to use in the Number Guessing Game.
    We recently learned about two new libraries and used them to create a Number Guessing Game (well done to us!). Anyway, it looks fun.

---
+++ Games/Sayı_Tahmin_Oyunu.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>                                  // Rastgele sayı üretimi için gerekli kütüphane
+#include <ctime>                                    // Zaman bilgisi için gerekli kütüphane
+
+using namespace std;
+
+int main() {
+    srand(time(0));                                 // Rastgele sayı üretimi için tohumlama
+    int rastgeleSayi = rand() % 100 + 1;            // 1 ile 100 arasında rastgele bir sayı
+    int tahmin;
+    int denemeSayisi = 0;
+
+    cout << "1 ile 100 arasında bir sayı tahmin edin: ";
+
+    do {
+        cin >> tahmin;
+        denemeSayisi++;
+
+        if (tahmin < rastgeleSayi) {
+            cout << "Daha yüksek bir sayı deneyin." << endl;
+        } else if (tahmin > rastgeleSayi) {
+            cout << "Daha düşük bir sayı deneyin." << endl;
+        } else {
+            cout << "Tebrikler! Sayıyı " << denemeSayisi << " denemede buldunuz." << endl;
+        }
+    } while (tahmin != rastgeleSayi);
+
+    return 0;
+}
